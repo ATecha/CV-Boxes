1355b35ab663e2deb0293a1c05657c1a2a1b044f
randolin <aaron.g.holloway@gmail.com>
2018-11-29 13:46:21 -0800

    Multi-file processing.

--- CV-Boxes/test.cpp
+++ CV-Boxes/test.cpp
@@ -8,7 +8,37 @@ using namespace cv;
 #include <iostream>
 using namespace std;
 
+void processImage(Mat& image) {
+
+}
+
 int main(int argc, const char* const argv[]) {
+
+	//-- Branch Based on Command Line Arguments --//
+	if (argc > 1) {
+		for (int index = 1; index < argc; index++) {  // Process each image.
+			cout << "Processing '" << argv[index] << "' ..." << endl;
+
+			Mat inputImage = imread(argv[index]);
+			if (inputImage.empty()) { // In case of imread failure (invalid filename, format, etc).
+				cout << "Unable to process '" << argv[index] << "'" << endl;
+			}
+			else {
+				processImage(inputImage);
+				cout << "Processed '" << argv[index] << "' successfully." << endl;
+			}
+		}
+	} else { // Process test.jpg if no files are specified.
+		cout << "Processing 'test.jpg' ..." << endl;
+
+		Mat inputImage = imread("test.jpg");
+		if (inputImage.empty()) {
+			cout << "Unable to process 'test.jpg'" << endl;
+		} else {
+			processImage(inputImage);
+			cout << "Processed 'test.jpg' successfully." << endl;
+		}
+	}
 
 	//-- End Program --//
 	return 0;
