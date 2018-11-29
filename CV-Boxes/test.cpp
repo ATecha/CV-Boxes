586e5b524431d7b48bd1c98a55e4228095e7cf09
randolin <aaron.g.holloway@gmail.com>
2018-11-29 13:26:24 -0800

    Got OpenCV library installed in project.

---
+++ CV-Boxes/test.cpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+using namespace cv;
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char* const argv[]) {
+
+	//-- End Program --//
+	return 0;
+
+}
